063e40f481d3b5dae44be7bd44216c70db8c66b5
masatotezuka <tez.0731.mst@gmail.com>
2023-11-01 00:29:53 +0900

    add constant

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const ESEARCH_API_URL =
+  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+export const EFETCH_API_URL =
+  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+export const PUBMED_BASE_URL = "https://pubmed.ncbi.nlm.nih.gov/"
