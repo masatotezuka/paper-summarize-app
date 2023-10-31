b1acb5bce9b2b857f0a0f306f3ccef666bca2216
masatotezuka <tez.0731.mst@gmail.com>
2023-11-01 00:29:09 +0900

    fix pubmed repo

--- repositories/pubmed.repository.ts
+++ repositories/pubmed.repository.ts
@@ -1,21 +1,40 @@
-export type ScrapingResponse = {
-  title: string
-  abstract: string
-  pubmedUrl: string
-  originUrl: string
+export type SearchResult = {
+  ids: number[]
 }
 
+export type FetchSummaryResult = {
+  abstract: string
+  title: string
+  url: string
+}
 export const pubmedRepository = {
-  async findPaperInfoBySearchWords({
+  async search({
     searchWords,
   }: {
-    searchWords: string[]
-  }): Promise<ScrapingResponse[]> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pubmed`, {
-      method: "POST",
-      body: JSON.stringify({ searchWords }),
-    })
+    searchWords: string
+  }): Promise<SearchResult> {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/pubmed/esearch`,
+      {
+        method: "POST",
+        body: JSON.stringify({ searchWords }),
+      }
+    )
     const data = await res.json()
-    return data.result
+    return data.results
+  },
+
+  async fetchSummary({ id }: { id: number }): Promise<FetchSummaryResult> {
+    console.log(id)
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/pubmed/efetch`,
+      {
+        method: "POST",
+        body: JSON.stringify({ id }),
+      }
+    )
+    const data = await res.json()
+    return data.results
   },
 }
